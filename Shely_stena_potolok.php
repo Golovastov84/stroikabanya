630ce53c25932d07a90b673588c3ed82bdea4e37
Vasya Golovastov <vsgolovastov@gmail.com>
2021-12-06 09:28:48 +0400

    add fail sait 0928 061221

---
+++ Shely_stena_potolok.php
@@ -0,0 +1,56 @@
+﻿<html>
+<head>
+<title>Щели</title>
+<meta charset=UTF-8/>
+<link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="script.js"></script>
+</head>
+<body text="#330000" bgcolor="#ccccc">
+<div class="page">
+<div class="shapka">
+<a id="zagolovok" onMouseOver="zagolovoks();">Заделывание щелей между стеной и потолком.</a>
+<span id="ssilka">
+<a href="pechka.php"><img src="bake.JPG" width="100"height="60"vspace="5" hspace="5"></a>
+<span id="content">
+<img src="content.JPG"width="100"height="60"vspace="5" hspace="5" onMouseOver="contents();">
+</span>
+<ol id="index-x">
+<?php
+include('menu.html');
+?>
+</ol>
+<a href="Lby_krishy.php"><img src="gogo.JPG"width="100"height="60"vspace="5" hspace="5"></a>
+</span>
+
+</div>
+<div onMouseOver="zagolovoks();">
+<div class="reklama">
+<a href="https://mail.ru"><img src="Mail.jpg" width="100"height="60"vspace="5" hspace="5"></a>
+</div>
+<div class="infa">
+<p>Так как потолок выполняет функцию пола у мансарды, его настилали поверх всех брёвен перекрытий.
+Из-за этого образовались щели между стеной и потолком.
+Изнутри заделали 40мм половой доской с джутовым уплотнением.</p>
+<img src="Shely_potolok_1.JPG"align="middle" vspace="5" hspace="5">
+<p>Внутри обшивается пароизоляцией и укладывается теплоизолятор пенополистирол. Затем, снова пароизоляция.
+ Со стороны улицы, закрывается 20мм доской.</p>
+<img src="Shely_potolok_2.JPG"align="middle" vspace="5" hspace="5">
+<p>Вид со стороны улицы.</p>
+<img src="Shely_potolok_3.JPG"align="middle" vspace="5" hspace="5">
+<p>Для заливки воды в баню, установили закрывающийся пробками канал для шланга.</p>
+<img src="Kanal_dlya_shlanga.JPG"align="middle" vspace="5" hspace="5">
+</div>
+</div>
+<div class="end">
+<a href="pechka.php"><img src="bake.JPG" width="100"height="60"vspace="5" hspace="5"></a>
+<a href="index.html"><img src="home.JPG" width="100"height="60"vspace="5" hspace="5"  onMouseOver="ends();"></a>
+<a href="Lby_krishy.php"><img src="gogo.JPG"width="100"height="60"vspace="5" hspace="5"></a>
+<ol id="index-y">
+<?php
+include('menu.html');
+?>
+</ol>
+</div>
+</div>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,6 @@
+function contents(){
+  document.getElementById("index-x").style.visibility='visible';
+ }
+  function zagolovoks(){
+  document.getElementById("index-x").style.visibility='hidden';
+ }
